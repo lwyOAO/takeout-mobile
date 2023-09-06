f2fe86fa3e1de530d10b9fe84112ddeb73336b7e
lwyOAO <2205217636@qq.com>
2023-09-06 09:59:45 +0800

    搭建好架子

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="box">你好</div>
+  <van-button type="primary">主要按钮</van-button>
+</template>
+
+<style lang="scss" scoped>
+.box {
+  height: 300px;
+  width: 300px;
+  background-color: aqua;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from '@vant/auto-import-resolver'
+import pxtovw from 'postcss-px-to-viewport'
+
+// postcss配置
+const loader_pxtovw = pxtovw({
+  viewportWidth: 320,
+  viewportUnit: 'vw'
+})
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()]
+    })
+  ],
+  css: {
+    postcss: {
+      plugins: [loader_pxtovw]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
